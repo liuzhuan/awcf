63a9a7047fc2e30e35d7f46c34ced4a2d48d87fd
liuz <liuzhuan@qingsongchou.com>
2017-07-28 18:21:29 +0800

    add canvas demo

---
+++ test/canvas/index.js
@@ -0,0 +1,42 @@
+let ctx;
+
+Page({
+  data: {
+    hidden: true
+  },
+
+  onLoad() { console.log('on load'); },
+  onShow() { console.log('on show'); },
+  onReady() {
+    ctx = wx.createCanvasContext('firstCanvas');
+    ctx.setStrokeStyle('black');
+    ctx.setLineWidth(4);
+  },
+
+  start(e) {
+    this.setData({
+      hidden: false,
+      x: e.touches[0].x,
+      y: e.touches[0].y
+    });
+
+    ctx.beginPath();
+    ctx.moveTo(e.touches[0].x, e.touches[0].y);
+  },
+  move(e) {
+    this.setData({
+      x: e.touches[0].x,
+      y: e.touches[0].y
+    })
+
+    ctx.lineTo(e.touches[0].x, e.touches[0].y);
+    // ctx.stroke();
+
+    ctx.draw(true);
+  },
+  end(e) {
+    this.setData({
+      hidden: true
+    });
+  }
+})
